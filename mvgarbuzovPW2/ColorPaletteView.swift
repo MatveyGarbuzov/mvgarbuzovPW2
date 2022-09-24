2c7319d30b99e436d52c9395d3a687d8082786bd
MatveyGarbuzov <matveyvoronezh@mail.ru>
2022-09-24 15:44:23 +0300

    Point 4 done.

--- mvgarbuzovPW2/ColorPaletteView.swift
+++ mvgarbuzovPW2/ColorPaletteView.swift
@@ -27,3 +27,45 @@ final class ColorPaletteView: UIControl {
     
   }
 }
+
+extension ColorPaletteView {
+  private final class colorSliderView: UIControl {
+    private let slider = UISlider()
+    private let colorLabel = UILabel()
+    
+    private(set) var value: Float = 0.0
+    
+    init(colorName: String, value: Float) {
+      self.value = value
+      super.init(frame: .zero)
+      
+      slider.value = value
+      colorLabel.text = colorName
+      setupView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+      let stackView = UIStackView(arrangedSubviews: [colorLabel, slider])
+      stackView.axis = .horizontal
+      stackView.spacing = 8
+      stackView.backgroundColor = .green
+      
+      addSubview(stackView)
+      stackView.snp.makeConstraints { make in
+        make.edges.equalToSuperview().inset(
+          UIEdgeInsets(
+            top: 16,
+            left: 16,
+            bottom: 16,
+            right: 16
+          )
+        )
+      }
+    }
+  }
+}
